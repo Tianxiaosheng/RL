31c0558b5852bc80b779d2eac50709fe754d0a58
xiaoshengt <xiaosheng.tian@uisee.com>
2024-02-18 16:50:14 +0800

    End the learning of Chapter 5

--- chapter5-test/taxi-v3.py
+++ chapter5-test/taxi-v3.py
@@ -114,17 +114,64 @@ class QLearningAgent:
         self.q[state, action] += self.learning_rate * td_error
 
 
-agent = ExpectedSARSAAgent(env)
+class DoubleQLearningAgent:
+    def __init__(self, env, gamma=0.9, learning_rate=0.1, epsilon=.01):
+        self.gamma = gamma
+        self.learning_rate = learning_rate
+        self.epsilon = epsilon
+        self.action_n = env.action_space.n
+        self.q0 = np.zeros((env.observation_space.n, env.action_space.n))
+        self.q1 = np.zeros((env.observation_space.n, env.action_space.n))
 
+    def decide(self, state):
+        if np.random.uniform() > self.epsilon:
+            action = (self.q0 + self.q1)[state].argmax()
+        else:
+            action = np.random.randint(self.action_n)
+        return action
+
+    def learn(self, state, action, reward, next_state, done):
+        if np.random.randint(2):
+            self.q0, self.q1 = self.q1, self.q0
+        a = self.q0[next_state].argmax()
+        u = reward + self.gamma * self.q1[next_state, a] * (1. - done)
+        td_error = u - self.q0[state, action]
+        self.q0[state, action] += self.learning_rate * td_error
+
+class SARSALambdaAgent(SARSAAgent):
+    def __init__(self, env, lambd=0.6, beta=1., gamma=0.9, learning_rate=0.1, epsilon=.01):
+        super().__init__(env, gamma=gamma, learning_rate=learning_rate, epsilon=epsilon)
+        self.lambd = lambd
+        self.beta = beta
+        self.e = np.zeros((env.observation_space.n, env.action_space.n))
+
+    def learn(self, state, action, reward, next_state, done, next_action):
+        # 更新资格轨迹
+        self.e *= (self.lambd * self.gamma)
+        self.e[state, action] = 1. + self.beta * self.e[state, action]
+
+        # 更新价值
+        u = reward + self.gamma * self.q[next_state, next_action] * (1. - done)
+        td_error = u - self.q[state, action]
+        self.q += self.learning_rate * self.e * td_error
+        #self.q[state, action] += self.learning_rate  * td_error
+        if done:
+            self.e *= 0.
+
+
+agent = SARSALambdaAgent(env)
+
+# 训练
 episodes = 5000
 episode_rewards = []
 for episode in range(episodes):
-    episode_reward = play_qlearning(env, agent, train=True)
+    episode_reward = play_sarsa(env, agent, train=True)
     episode_rewards.append(episode_reward)
 
 plt.plot(episode_rewards)
 plt.show()
-agent.epsilon = 0.
 
-episode_rewards = [play_qlearning(env, agent) for _ in range(100)]
+# 测试
+agent.epsilon = 0. # 取消探索
+episode_rewards = [play_sarsa(env, agent) for _ in range(100)]
 print('平均回合奖励 = {} / {} = {}'.format(sum(episode_rewards), len(episode_rewards), np.mean(episode_rewards)))
